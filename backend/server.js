c2cbd9a69e41bb74b21e1a602ac035dceda43937
Md Imran <imran33s786@gmail.com>
2025-10-16 20:22:50 +0530

    Remove bcrypt dependency

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+ 
 const app = express();
 app.use(cors()); // Allow all origins
 app.use(bodyParser.json());
