66e07ebcb31eeecb2146be20c62377a1fc2876fa
Md Imran <imran33s786@gmail.com>
2025-12-18 18:36:08 +0530

    fixed config issue

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 ======================= */
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://my-blog-s.netlify.app",
+  "https://newsmanagementsystem.netlify.app",
 ];
 
 app.use(
